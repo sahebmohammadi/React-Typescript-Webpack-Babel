89b462c87078198dba7be04980d21c0b31bcb281
saheb mohammadi <saheb.put92@gmail.com>
2022-07-14 12:20:01 +0430

    fix webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
